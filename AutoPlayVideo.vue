c6e7170a6071da5d89065e28be7ed28c30a42ff0
lihe07 <li@imlihe.com>
2023-10-16 10:36:03 +0800

    init, AutoPlayVideo

---
+++ AutoPlayVideo.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {watchEffect,ref} from "vue"
+  const props = defineProps(["src"]);
+  
+  const ele = ref(null)
+  function onEnter() {
+    if (!ele.value) return
+    console.log("ref", ele.value)
+    ele.value.currentTime = 0
+    ele.value.play()
+  }
+
+  watchEffect(() => {
+    if ($page.value === $slidev.nav.currentPage) onEnter()
+  });
+  
+</script>
+
+<template>
+  <video :src="props.src" ref="ele"  />
+</template>
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # slidev-snippets
 Some handful snippets for slidev presentations
+
+## AutoPlayVideo
+
+A `<video />` that is auto triggered at page entrance.
+
+### Usage
+
+```md
+---
+<AutoPlayVideo src="https://example.com/video.mp4" />
+---
+<AutoPlayVideo src="https://example.com/video2.mp4" />
+---
+```
